52cd00354ac8f342191c7096ae406eb59fd6f14b
AElisee <2kange.eli@gamil.com>
2022-08-06 10:47:50 +0000

    pratique et code source

--- code_source/yoga-routine/index.js
+++ code_source/yoga-routine/index.js
@@ -1,4 +1,5 @@
 const main = document.querySelector("main");
+
 const basicArray = [
   { pic: 0, min: 1 },
   { pic: 1, min: 1 },
@@ -11,12 +12,10 @@ const basicArray = [
   { pic: 8, min: 1 },
   { pic: 9, min: 1 },
 ];
+
 let exerciceArray = [];
 
-// fonction anonyme sui se lance tout seule au chargement de de las page
-//  renvoie les données stocker dans le localeStorage si elles existent
-//  sinon renvoie le tableau de base
-
+// Get stored exercices array
 (() => {
   if (localStorage.exercices) {
     exerciceArray = JSON.parse(localStorage.exercices);
@@ -24,8 +23,6 @@ let exerciceArray = [];
     exerciceArray = basicArray;
   }
 })();
-
-// ------------------------
 
 class Exercice {
   constructor() {
@@ -38,9 +35,9 @@ class Exercice {
     this.seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
 
     setTimeout(() => {
-      if (this.minutes === 0 && this.seconds == "00") {
-        this.ring();
+      if (this.minutes === 0 && this.seconds === "00") {
         this.index++;
+        this.ring();
         if (this.index < exerciceArray.length) {
           this.minutes = exerciceArray[this.index].min;
           this.seconds = 0;
@@ -48,7 +45,7 @@ class Exercice {
         } else {
           return page.finish();
         }
-      } else if (this.seconds == "00") {
+      } else if (this.seconds === "00") {
         this.minutes--;
         this.seconds = 59;
         this.updateCountdown();
@@ -56,17 +53,14 @@ class Exercice {
         this.seconds--;
         this.updateCountdown();
       }
-    }, 100);
+    }, 1000);
 
     return (main.innerHTML = `
-    <div class="exercice-container">
-     <p>${this.minutes}:${this.seconds}
-    </p>
-     <img src="./img/${exerciceArray[this.index].pic}.png" />
-    <div>${this.index + 1}/${exerciceArray.length}
-    </div>
-    </div>
-    `);
+      <div class="exercice-container">
+        <p>${this.minutes}:${this.seconds}</p>
+        <img src="./img/${exerciceArray[this.index].pic}.png" />
+        <div>${this.index + 1}/${exerciceArray.length}</div>
+      </div>`);
   }
 
   ring() {
@@ -84,7 +78,7 @@ const utils = {
   },
 
   handleEventMinutes: function () {
-    document.querySelectorAll("input[type='number']").forEach((input) => {
+    document.querySelectorAll('input[type="number"]').forEach((input) => {
       input.addEventListener("input", (e) => {
         exerciceArray.map((exo) => {
           if (exo.pic == e.target.id) {
@@ -101,7 +95,7 @@ const utils = {
       arrow.addEventListener("click", (e) => {
         let position = 0;
         exerciceArray.map((exo) => {
-          if (exo.pic == e.target.dataset.pic && position != 0) {
+          if (exo.pic == e.target.dataset.pic && position !== 0) {
             [exerciceArray[position], exerciceArray[position - 1]] = [
               exerciceArray[position - 1],
               exerciceArray[position],
@@ -120,7 +114,7 @@ const utils = {
     document.querySelectorAll(".deleteBtn").forEach((btn) => {
       btn.addEventListener("click", (e) => {
         let newArr = [];
-        exerciceArray.filter((exo) => {
+        exerciceArray.map((exo) => {
           if (exo.pic != e.target.dataset.pic) {
             newArr.push(exo);
           }
@@ -173,10 +167,12 @@ const page = {
     reboot.addEventListener("click", () => utils.reboot());
     start.addEventListener("click", () => this.routine());
   },
+
   routine: function () {
     const exercice = new Exercice();
     utils.pageContent("Routine", exercice.updateCountdown(), null);
   },
+
   finish: function () {
     utils.pageContent(
       "C'est terminé !",
